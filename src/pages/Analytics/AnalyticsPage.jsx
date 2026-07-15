b006c2a06889f4791645aa343f6b78bac1747981
yussif4112mohammed <yussif4112@gmailcom>
2026-07-15 19:27:21 +0000

    fix(analytics): import useEffect in AnalyticsPage

--- src/pages/Analytics/AnalyticsPage.jsx
+++ src/pages/Analytics/AnalyticsPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SegmentedControl from '../../components/SegmentedControl/SegmentedControl';
 import { photographers, PHOTO_URLS } from '../../data/photographers';
 import './AnalyticsPage.css';
